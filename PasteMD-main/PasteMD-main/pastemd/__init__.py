f1b2a0dc195b68df84eed0ad6d6c0a102abcea7b
A Good Man <972997831@qq.com>
2025-12-11 22:51:36 +0800

    修改

--- PasteMD-main/PasteMD-main/pastemd/__init__.py
+++
@@ -1,4 +0,0 @@
-"""PasteMD - Markdown to DOCX conversion with hotkey support."""
-
-__version__ = "0.1.6.0"
-__app_name__ = "PasteMD"
--- PasteMD-main/PasteMD-main/pastemd/domains/spreadsheet/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Spreadsheet insertion domain."""
-
-from .excel import MSExcelInserter
-from .wps_excel import WPSExcelInserter
-
-__all__ = ["MSExcelInserter", "WPSExcelInserter"]
